3a8efb91378cecf871fa8f1f1ea2895472df6c91
Rob Johnson <rjohnson@dcs_inc.net>
2022-04-28 22:36:19 -0700

    implement some of the higher level display functions.

--- Core/Src/display.c
+++ Core/Src/display.c
@@ -6,17 +6,24 @@
  */
 
 #include "display.h"
+#include <string.h>
 
 void display_init(void) {
 	// todo
 }
 
 void display_clear_all(void) {
-	// todo
+	for(uint8_t row = 0; row < MAX_ROW; ++row) {
+		display_clear_row(row);
+	}
 }
 
 void display_clear_row(uint8_t row) {
-	// todo
+	char blank_data[MAX_COLUMN];
+	memset(blank_data, ' ', MAX_COLUMN);
+
+	display_set_cursor(row, 0);
+	display_write(blank_data);
 }
 
 void display_set_cursor(uint8_t row, uint8_t column) {
