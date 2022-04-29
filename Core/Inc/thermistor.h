3d822684051d19135fcbc18b421248660e7e8af6
Rob Johnson <rjohnson@dcs_inc.net>
2022-04-28 22:15:40 -0700

    change interface to thermistor to hold units internally

--- Core/Inc/thermistor.h
+++ Core/Inc/thermistor.h
@@ -11,6 +11,10 @@
 #include <math.h>
 #include <stdbool.h>
 
+#define UNITS_KELVIN 		("K")
+#define UNITS_FAHRENHEIT 	("F")
+#define UNITS_CELSIUS 		("C")
+
 /*
  * Initialize the thermistor module.
  */
@@ -28,14 +32,23 @@ void thermistor_begin_reading(void);
 bool thermistor_reading_complete(void);
 
 /*
- * Return the thermistor temperature reading in the specifed units of each
- * 	function with 1 decimal place of accuracy.
+ * Return the thermistor temperature reading in the units specifed with 1
+ *  decimal place of accuracy.
  *
  * If the module is in an error state the functions will return NaN.
 */
-float_t thermistor_get_reading_c(void);
-float_t thermistor_get_reading_f(void);
-float_t thermistor_get_reading_k(void);
+float_t thermistor_get_reading(void);
 
+/*
+ * Get a string specifying the current units to represent readings.
+ */
+char* thermistor_get_units(void);
+
+/*
+ * Set the current units to represent readings. Valid inputs are
+ *  ['K', 'F', 'C'] for kelvin, fahrenheit, and celsius respectively.
+ *  If a different value is given then no change shall be made.
+ */
+void thermistor_set_units(char* newUnits);
 
 #endif /* THERMISTOR_H_ */
--- Core/Src/thermistor.c
+++ Core/Src/thermistor.c
@@ -5,27 +5,92 @@
  *      Author: RAJ
  */
 #include "thermistor.h"
+#include <string.h>
+
+// private variables
+enum unitsEnum {
+	celsius,
+	fahrenheit,
+	kelvin
+};
+static enum unitsEnum units;
+
+// private function declarations
+static float_t thermistor_get_reading_c(void);
+static float_t thermistor_get_reading_f(void);
+static float_t thermistor_get_reading_k(void);
 
 void thermistor_init(void) {
-	// todo
+	// todo - init adc
 }
 
 void thermistor_begin_reading(void) {
-	// todo
+	// todo -
 }
 
 bool thermistor_reading_complete(void) {
 	return true;	// todo
 }
 
-float_t thermistor_get_reading_c(void) {
+float_t thermistor_get_reading(void) {
+	float_t result;
+	switch(units) {
+	case fahrenheit:
+		result = thermistor_get_reading_f();
+		break;
+	case kelvin:
+		result = thermistor_get_reading_k();
+		break;
+	case celsius:
+		result = thermistor_get_reading_c();
+		break;
+	default:
+		result = thermistor_get_reading_c();
+		break;
+	}
+	return result;
+}
+
+char* thermistor_get_units(void) {
+	char* result;
+	switch(units) {
+	case fahrenheit:
+		result = UNITS_FAHRENHEIT;
+		break;
+	case kelvin:
+		result = UNITS_KELVIN;
+		break;
+	case celsius:
+		result = UNITS_CELSIUS;
+		break;
+	default:
+		result = UNITS_CELSIUS;
+		break;
+	}
+	return result;
+}
+
+void thermistor_set_units(char* newUnits) {
+	if(strlen(newUnits) != 1) {
+		return;
+	}
+	if(strcmp(newUnits, UNITS_CELSIUS) == 0) {
+		units = celsius;
+	} else if(strcmp(newUnits, UNITS_KELVIN) == 0) {
+		units = kelvin;
+	} else if(strcmp(newUnits, UNITS_FAHRENHEIT) == 0) {
+		units = fahrenheit;
+	}
+}
+
+static float_t thermistor_get_reading_c(void) {
 	return 100;	// todo
 }
 
-float_t thermistor_get_reading_f(void) {
+static float_t thermistor_get_reading_f(void) {
 	return (thermistor_get_reading_c() * 9 / 5) + 32;	// todo round
 }
 
-float_t thermistor_get_reading_k(void) {
+static float_t thermistor_get_reading_k(void) {
 	return thermistor_get_reading_c() + 273.15;	// todo round
 }
